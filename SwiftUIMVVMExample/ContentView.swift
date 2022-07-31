657ffc3470bf906704e3d60ebfefcc1625b23265
장효원 <hyo961015@naver.com>
2022-07-31 11:15:36 +0900

    UIHostingController 추가

---
+++ SwiftUIMVVMExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIMVVMExample
+//
+//  Created by 장효원 on 2022/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("test")
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
